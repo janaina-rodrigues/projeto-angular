d424e819dcd9adc6c13b61c5b78cd2280d932494
janaina-rodrigues <janaahrodrigues@gmail.com>
2019-12-26 18:29:01 -0200

    teste criação de componente

---
+++ src/app/app-hello/app-hello.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './app-hello.component.html',
+  styleUrls: ['./app-hello.component.css']
+})
+export class AppHelloComponent  {
+
+  nome = 'janaina'; 
+}
